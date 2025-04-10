9ef705143a050f2ddaba9ab167a277ddcf7b7dab
ALT <andersonlottermann@gmail.com>
2025-04-10 16:53:52 -0300

    Remove individual exports for HPP and XSS.

--- backend/src/middleware/security.ts
+++ backend/src/middleware/security.ts
@@ -1,11 +1,5 @@
 import hpp from 'hpp';
 import xss from 'xss-clean';
 
-// HTTP Parameter Pollution protection
-export const hppProtection = hpp();
-
-// XSS Protection
-export const xssProtection = xss();
-
 // Request Sanitization
 export const sanitizeRequest = [xss(), hpp()];
